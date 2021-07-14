5a50cea79972b1433affc01283b1e41476da642c
Freddy Canido <freddy.canido12@gmail.com>
2021-07-13 20:32:29 -0400

    simplified menu

--- src/TableTopGames/Menu.java
+++ src/TableTopGames/Menu.java
@@ -7,24 +7,7 @@ public class Menu {
 
     public static void main(String[] args) {
         // write your code here
-        Scanner scan = new Scanner(System.in);
-
-
-        System.out.println("How many players will play?");
-        int playerNum = scan.nextInt();
-        scan.nextLine();
-
-        for (int i = 0; i < playerNum; i++) {
-
-            System.out.println("Enter name");
-            String name = scan.nextLine();
-            System.out.println("Enter color");
-            String color = scan.nextLine();
-
-            Player player = new Player(name, color);
-            Turn.addPlayer(player);
-            Player.getDice(player);
-        }
+        Turn.createPlayers();
         Turn.setTurns();
         while (Turn.turnMax != 0) {
             Turn.displayTurn();
--- src/TableTopGames/Turn.java
+++ src/TableTopGames/Turn.java
@@ -2,13 +2,33 @@ package TableTopGames;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Turn {
     public static int turnCounter = 0;
     public static List<Player> players = new ArrayList<>();
 
     public static int turnMax = 0;
+    public static void createPlayers(){
+        Scanner scan = new Scanner(System.in);
 
+
+        System.out.println("How many players will play?");
+        int playerNum = scan.nextInt();
+        scan.nextLine();
+
+        for (int i = 0; i < playerNum; i++) {
+
+            System.out.println("Enter name");
+            String name = scan.nextLine();
+            System.out.println("Enter color");
+            String color = scan.nextLine();
+
+            Player player = new Player(name, color);
+            Turn.addPlayer(player);
+            Player.getDice(player);
+        }
+    }
     public static void addPlayer(Player player) {
         players.add(player);
     }
