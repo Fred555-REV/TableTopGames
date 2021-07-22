d1bec68fd05ecf7a7c0adafdc72f047286c1b3ce
Freddy Canido <freddy.canido12@gmail.com>
2021-07-22 19:58:30 -0400

    added min and max players

--- src/TableTopGames/Turn.java
+++ src/TableTopGames/Turn.java
@@ -2,21 +2,26 @@ package TableTopGames;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.MissingFormatArgumentException;
 import java.util.Scanner;
 
 public class Turn {
     public static int turnCounter = 0;
     public static List<Player> players = new ArrayList<>();
-
+    public static final int MAX_PLAYERS = 6;
+    public static final int MIN_PLAYERS = 1;
     public static int turnMax = 0;
 
     public static void createPlayers() {
         Scanner scan = new Scanner(System.in);
         System.out.println("How many players will there be?");
-        int playerNum = scan.nextInt();
-        scan.nextLine();
+        int playerNum;
+        do {
+            playerNum = scan.nextInt();
+            scan.nextLine();
+        } while (playerNum < MIN_PLAYERS || playerNum > MAX_PLAYERS);
 
-        for (int i = 0; i < playerNum; i++) {
+        while (players.size() < playerNum) {
 
             System.out.println("Enter name");
             String name = scan.next().trim();
@@ -30,11 +35,13 @@ public class Turn {
 
     public static void createPlayers(int scoreAmount, int diceAmount, int numOfSides) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("How many players will there be?");
-        int playerNum = scan.nextInt();
-        scan.nextLine();
+        int playerNum;
+        do {
+            playerNum = scan.nextInt();
+            scan.nextLine();
+        } while (playerNum < MIN_PLAYERS || playerNum > MAX_PLAYERS);
 
-        for (int i = 0; i < playerNum; i++) {
+        while (players.size() < playerNum) {
 
             System.out.println("Enter name");
             String name = scan.next().trim();
@@ -74,7 +81,7 @@ public class Turn {
     }
 
     public static void displayTurn() {
-        System.out.println(players.get(turnCounter).name + "'s or " + players.get(turnCounter).color + "'s turn ");
+        System.out.println(getActivePlayer().name + "'s or " + getActivePlayer().color + "'s turn ");
     }
 
     public static void displayTurnsLeft() {
